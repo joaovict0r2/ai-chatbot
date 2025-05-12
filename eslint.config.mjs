485af0d903d80890fceaee1e7e4c2e67194eeb3d
joaovict0r <joaovictorcsantos13@gmail.com>
2025-05-11 22:10:47 -0300

    finally fix eslint build error

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -12,11 +12,22 @@ const compat = new FlatCompat({
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
+    ignores: [
+      "**/node_modules/**",
+      ".next/**",
+      "out/**",
+      "build/**",
+      "dist/**",
+      "public/**",
+      "**/generated/**",
+    ],
+  },
+  {
     files: ["**/*.{ts,tsx}"],
     rules: {
       '@typescript-eslint/no-explicit-any': 'off'
     }
-  }
+  },
 ];
 
 export default eslintConfig;
